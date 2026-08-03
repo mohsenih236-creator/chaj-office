efeb94e6fe5617c793aca25f5edfc039d2579109
mohsenih236-creator <mohsenih236@gmail.com>
2026-08-03 18:24:18 +0330

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -262,8 +262,8 @@ export const projects: Project[] = [
         galleryImages: [
           "/images/shirvani-d1-render02.jpg",
           "/images/shirvani-d1-details01.jpg",
-          "public/images/shirvani-d1-details02.jpg",
-          "/images/shirvani-detail-01d.jpg"
+          "/images/shirvani-d1-details02.jpg",
+          "/images/shirvani-d1-details03.jpg"
         ]
       },
       {
