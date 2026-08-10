aab3098f916c72eeca9b102b48593d6858c09b5d
mohsenih236-creator <mohsenih236@gmail.com>
2026-08-10 17:38:10 +0330

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1039,7 +1039,7 @@ export const projects: Project[] = [
 
         galleryImages: [
           "/images/MDF-EX-Excavation02.jpg",
-          "/images/MDF-EX-Excavation03",
+          "/images/MDF-EX-Excavation03.jpg",
           "/images/MDF-EX-Excavation04",
           "/images/MDF-EX-Excavation05"
         ]
