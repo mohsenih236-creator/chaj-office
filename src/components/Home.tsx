b8410655887d334c0f50b1195ebfbe1ca37f2d14
mohsenih236-creator <mohsenih236@gmail.com>
2026-08-15 18:00:50 +0330

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -90,8 +90,8 @@ const CrossfadeStack: React.FC<{
         18deg,
         rgba(0,0,0,0) 0%,
         rgba(0,0,0,0.15) 7%,
-        rgba(0,0,0,1) 20%,
-        rgba(0,0,0,1) 80%,
+        rgba(0,0,0,1) 5%,
+        rgba(0,0,0,1) 95%,
         rgba(0,0,0,0.15) 93%,
         rgba(0,0,0,0) 100%
       ),
